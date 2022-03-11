fc968ac850e922dd99bf18880189af8c7b5495f0
Bethpen <64626802+Bethpen@users.noreply.github.com>
2022-03-11 10:10:22 +0100

    Updates

---
+++ precache-manifest.7b7625f60b40ca6afff63feaa7eb6e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a6ca90dc0d4e4b26b8b2a3f56b22c3",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "5f7b126e4df27f65fc20",
+    "url": "/cart/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/cart/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7b126e4df27f65fc20",
+    "url": "/cart/static/js/main.0d0c399f.chunk.js"
+  },
+  {
+    "revision": "6f930286e52d694f2280",
+    "url": "/cart/static/js/runtime-main.526553cb.js"
+  }
+]);
